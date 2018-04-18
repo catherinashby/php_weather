d1ef33cff91d0f96cb02804c204dd8401cd09cd6
Catherine <catherinashby@gmail.com>
2018-04-18 10:59:20 -0400

    added basic page

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    <meta http-equiv="content-script-type" content="text/javascript" />
+    <meta http-equiv="X-UA-Compatible" content="IE=9; IE=8; IE=7;" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta http-equiv="cache-control" content="no-cache" />
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta http-equiv="Expires" content="-1" />
+	<title>Weather</title>
+	<!-- css files -->
+	<!-- javascript files -->
+  </head>
+  <body>
+<?php
+	echo '<p>This page intentionally left blank.</p>';
+ ?>
+  </body>
+</html>
