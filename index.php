0c64a7b5642c501043f4f20662c2523e8de474c0
Catherine <catherinashby@gmail.com>
2018-04-18 11:37:52 -0400

    fetch json, display city

--- index.php
+++ index.php
@@ -13,8 +13,24 @@
 	<!-- javascript files -->
   </head>
   <body>
-<?php
-	echo '<p>This page intentionally left blank.</p>';
+ <?php
+  $json_string = file_get_contents("http://api.wunderground.com/api/fd33019fe645d261/conditions/q/PA/Philadelphia.json");
+  $parsed_json = json_decode($json_string);
+  $location = $parsed_json->{'current_observation'}->{'display_location'};
  ?>
+  <table>
+  <caption>
+	<?php  echo "Current conditions for ${location}->{'full'}"; ?>
+  </caption>
+    <thead>
+	  <tr><th /></tr>
+	</thead>
+	<tbody>
+	  <tr>
+	    <th>Label</th>
+		<td>Data</td>
+	  </tr>
+	</tbody>
+  </table>
   </body>
 </html>
